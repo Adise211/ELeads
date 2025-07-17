3207d174d2fd37c4a52b911fb012bb81cab53223
Adise <adisemamo211@gmail.com>
2025-07-17 14:47:04 -0400

    feat: add routes for creating and updating leads with validation and authentication

---
+++ server/controllers/leads.controller.ts
@@ -0,0 +1,47 @@
+import { Request, Response, NextFunction } from "express";
+import {
+  createLead as createLeadModel,
+  updateUserLead as updateUserLeadModel,
+} from "../models/leads.model";
+import { httpCodes } from "../utils/errorCodes";
+import { SuccessResponse } from "../server.types";
+
+export const createLead = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const { userId } = (req as any).user;
+    const { data } = req.body;
+    const createdLead = await createLeadModel(userId, data);
+    const successResponse: SuccessResponse = {
+      success: true,
+      message: "Lead created successfully",
+      data: createdLead,
+    };
+    res.status(httpCodes.SUCCESS).json(successResponse);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const updateUserLead = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const { userId } = (req as any).user;
+    const { leadId, data } = req.body;
+    const updatedLead = await updateUserLeadModel(userId, leadId, data);
+    const successResponse: SuccessResponse = {
+      success: true,
+      message: "Lead updated successfully",
+      data: updatedLead,
+    };
+    res.status(httpCodes.SUCCESS).json(successResponse);
+  } catch (error) {
+    next(error);
+  }
+};
---
+++ server/models/leads.model.ts
@@ -0,0 +1,29 @@
+import { Lead, PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+export const createLead = async (userId: string, data: Partial<Lead>) => {
+  // Ensure required fields are present and types are correct
+
+  const lead = await prisma.lead.create({
+    //TODO: fix this type error
+    // @ts-ignore
+    data: {
+      ...data,
+      assignedToId: userId,
+    },
+  });
+  return lead;
+};
+
+export const updateUserLead = async (
+  userId: string,
+  leadId: string,
+  data: Partial<Lead>
+) => {
+  const lead = await prisma.lead.update({
+    where: { id: leadId, AND: { assignedToId: userId } },
+    data,
+  });
+  return lead;
+};
