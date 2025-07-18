73ad7c21cece08ef98e5bfe3932d0f8bc42a700f
Adise <adisemamo211@gmail.com>
2025-07-18 13:22:34 -0400

    chore: remove unused root.types.ts file, update dev script in package.json, and add new DTO types for User, Workspace, Lead, Activity, and Note

---
+++ shared/types/index.ts
@@ -0,0 +1,56 @@
+import { ActivityType, UserRole } from "./prisma-enums";
+
+export type UserDTO = {
+  id?: string;
+  email: string;
+  firstName: string;
+  lastName: string;
+  role: UserRole;
+  isActive: boolean;
+  phone: string;
+  workspaceId: string;
+  workspace: WorkspaceDTO;
+  permissions: string[];
+  leads: LeadDTO[];
+  activities: ActivityDTO[];
+  createdAt?: Date;
+  updatedAt?: Date;
+};
+
+export type WorkspaceDTO = {
+  id?: string;
+  name: string;
+  description: string;
+  createdAt?: Date;
+  updatedAt?: Date;
+};
+
+export type LeadDTO = {
+  id?: string;
+  name: string;
+  email: string;
+  phone: string;
+  createdAt?: Date;
+  updatedAt?: Date;
+};
+
+export type ActivityDTO = {
+  id?: string;
+  type: ActivityType;
+  description: string;
+  leadId: string;
+  lead: LeadDTO;
+  userId: string;
+  user: UserDTO;
+  createdAt?: Date;
+  updatedAt?: Date;
+};
+
+export type NoteDTO = {
+  id?: string;
+  content: string;
+  leadId: string;
+  lead: LeadDTO;
+  createdAt?: Date;
+  updatedAt?: Date;
+};
