e34db6a4ac201e81c33c3084941559b862b20922
Adise <adisemamo211@gmail.com>
2025-07-20 15:09:37 -0400

    chore: configure Vite server port and add dotenv dependency for environment variables

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,7 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    port: Number(import.meta.env.VITE_PORT) || 8080,
+  },
+});
