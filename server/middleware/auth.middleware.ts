d4fbb15b225bb53657f36070dda92e770cb09610
Adise <adisemamo211@gmail.com>
2025-07-23 13:38:43 -0400

    refactor: enhance token retrieval in authenticateToken middleware to support cookies as fallback

--- server/middleware/auth.middleware.ts
+++ server/middleware/auth.middleware.ts
@@ -3,16 +3,16 @@ import { verifyAccessToken } from "../utils/auth.helper.js";
 import { AppError } from "../middleware/errorHandler.middleware.js";
 import { httpCodes } from "../utils/errorCodes.js";
 
-export function authenticateToken(
-  req: Request,
-  res: Response,
-  next: NextFunction
-) {
+export function authenticateToken(req: Request, res: Response, next: NextFunction) {
   try {
     // Check for the Authorization header
     const authHeader = req.headers["authorization"];
-    const token = authHeader?.split(" ")[1]; // Bearer <token>
-
+    let token = authHeader?.split(" ")[1]; // Bearer <token>
+    if (!token) {
+      // Try to get token from cookies
+      const accessTokenCookieName = process.env.COOKIE_ACCESS_TOKEN_NAME as string;
+      token = req.cookies?.[accessTokenCookieName];
+    }
     if (!token) {
       next(new AppError("No token provided", httpCodes.UNAUTHORIZED));
     } else {
