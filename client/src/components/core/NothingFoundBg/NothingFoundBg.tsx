35e46fa5114e09bbf00cd8b81379aa14002f344a
Adise <adisemamo211@gmail.com>
2025-07-25 12:50:36 -0400

    feat: refactor NothingFoundBg component to use Radix UI, enhance styling, and improve layout

--- client/src/components/core/NothingFoundBg/NothingFoundBg.tsx
+++ client/src/components/core/NothingFoundBg/NothingFoundBg.tsx
@@ -1,4 +1,4 @@
-import { Button, Container, Group, Text, Title } from "@mantine/core";
+import { Flex, Button } from "@radix-ui/themes";
 import Illustration from "@/assets/icons/Illustration";
 import classes from "./NothingFoundBg.module.css";
 import { useNavigate } from "react-router-dom";
@@ -7,23 +7,23 @@ const NothingFoundBg = () => {
   const navigate = useNavigate();
 
   return (
-    <Container className={`${classes.root} ${classes.fullScreen}`}>
+    <div className={`${classes.root} ${classes.fullScreen}`}>
       <div className={classes.inner}>
         <Illustration className={classes.image} />
         <div className={classes.content}>
-          <Title className={classes.title}>Nothing to see here</Title>
-          <Text c="dimmed" size="lg" ta="center" className={classes.description}>
+          <div className={classes.title}>Nothing to see here</div>
+          <div className={classes.description}>
             Page you are trying to open does not exist. You may have mistyped the address, or the
             page has been moved to another URL. If you think this is an error contact support.
-          </Text>
-          <Group justify="center">
-            <Button size="md" onClick={() => navigate("/home")}>
+          </div>
+          <Flex justify="center">
+            <Button size="3" onClick={() => navigate("/home")}>
               Take me back to home page
             </Button>
-          </Group>
+          </Flex>
         </div>
       </div>
-    </Container>
+    </div>
   );
 };
 
