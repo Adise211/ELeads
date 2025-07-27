cf890f2a8cea63438ebbdfb4f4951b79e7b1b953
Adise <adisemamo211@gmail.com>
2025-07-27 13:56:06 -0400

    feat: add dashboard components including CardsSection and ChartSection; enhance data visualization with Recharts and improve UI with new toggle and chart components

--- client/src/pages/HomePage.tsx
+++ client/src/pages/HomePage.tsx
@@ -1,38 +1,30 @@
-import {
-  AlertDialog,
-  AlertDialogContent,
-  AlertDialogDescription,
-  AlertDialogFooter,
-  AlertDialogCancel,
-  AlertDialogAction,
-  AlertDialogHeader,
-  AlertDialogTitle,
-  AlertDialogTrigger,
-} from "@/components/ui/alert-dialog";
-import { Button } from "@/components/ui/button";
+// import {
+//   AlertDialog,
+//   AlertDialogContent,
+//   AlertDialogDescription,
+//   AlertDialogFooter,
+//   AlertDialogCancel,
+//   AlertDialogAction,
+//   AlertDialogHeader,
+//   AlertDialogTitle,
+//   AlertDialogTrigger,
+// } from "@/components/ui/alert-dialog";
+// import { Button } from "@/components/ui/button";
+import ChartSection from "@/components/core/Dashboard/ChartSection";
+import CardsSection from "@/components/core/Dashboard/CardsSection";
 
 const HomePage = () => {
   return (
-    <div>
-      <h1>Home Page</h1>
-      <AlertDialog>
-        <AlertDialogTrigger asChild>
-          <Button variant="outline">Show Dialog</Button>
-        </AlertDialogTrigger>
-        <AlertDialogContent>
-          <AlertDialogHeader>
-            <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
-            <AlertDialogDescription>
-              This action cannot be undone. This will permanently delete your account and remove
-              your data from our servers.
-            </AlertDialogDescription>
-          </AlertDialogHeader>
-          <AlertDialogFooter>
-            <AlertDialogCancel>Cancel</AlertDialogCancel>
-            <AlertDialogAction>Continue</AlertDialogAction>
-          </AlertDialogFooter>
-        </AlertDialogContent>
-      </AlertDialog>
+    <div className="flex flex-1 flex-col">
+      <div className="@container/main flex flex-1 flex-col gap-2">
+        <div className="flex flex-col gap-4 py-4 md:gap-6 md:py-6">
+          <CardsSection />
+          <div className="px-4 lg:px-6">
+            <ChartSection />
+          </div>
+          {/* <DataTable data={data} /> */}
+        </div>
+      </div>
     </div>
   );
 };
