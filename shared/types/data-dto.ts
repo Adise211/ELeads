ab5cb892183d08de3fe1b85ddf44a0eabdb8ccc9
Adise <adisemamo211@gmail.com>
2025-08-04 10:02:17 -0400

    feat: moved data dtos to a different file and exported them from the index in shared folder

---
+++ shared/types/data-dto.ts
@@ -0,0 +1,74 @@
+import { ActivityType, LeadStatus, UserRole } from "./prisma-enums";
+
+// DTO stands for Data Transfer Object.
+// It is a simple object used to transfer data between different layers or parts of an application,
+// typically without any business logic.
+
+export type UserDTO = {
+  id?: string;
+  email: string;
+  firstName: string;
+  lastName: string;
+  role: UserRole;
+  avatarUrl: string | null;
+  isActive: boolean;
+  phone: string;
+  workspaceId: string;
+  workspace: WorkspaceDTO;
+  permissions: string[];
+  leads: LeadDTO[];
+  activities: ActivityDTO[];
+  createdAt?: Date;
+  updatedAt?: Date;
+};
+
+export type WorkspaceDTO = {
+  id?: string;
+  name: string;
+  description: string;
+  createdAt?: Date;
+  updatedAt?: Date;
+};
+
+export type LeadDTO = {
+  id?: string;
+  firstName: string;
+  lastName: string;
+  email: string;
+  phone: string[];
+  company: string;
+  jobTitle?: string;
+  industry?: string;
+  status: LeadStatus;
+  website?: string;
+  address?: string;
+  city?: string;
+  state?: string;
+  zipCode?: string;
+  country: string;
+  notes?: NoteDTO[];
+  activities?: ActivityDTO[];
+  createdAt?: Date;
+  updatedAt?: Date;
+};
+
+export type ActivityDTO = {
+  id?: string;
+  type: ActivityType;
+  description: string;
+  leadId?: string;
+  lead?: LeadDTO;
+  userId?: string;
+  user?: UserDTO;
+  createdAt?: Date;
+  updatedAt?: Date;
+};
+
+export type NoteDTO = {
+  id?: string;
+  content: string;
+  leadId?: string;
+  lead?: LeadDTO;
+  createdAt?: Date;
+  updatedAt?: Date;
+};
---
+++ shared/types/general.ts
@@ -0,0 +1,13 @@
+export interface SuccessResponse {
+  success: boolean;
+  message: string;
+  data?: any;
+}
+
+export interface ErrorResponse {
+  success: boolean;
+  status: string;
+  message: string;
+  error?: any;
+  stack?: string;
+}
