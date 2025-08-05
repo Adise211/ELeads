6e93177d3b37850c10a4c2869b706753d3936fa7
Adise <adisemamo211@gmail.com>
2025-08-05 12:29:26 -0400

    chore: add alias for shared package in TypeScript and Vite configuration

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -21,6 +21,7 @@ export default defineConfig({
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
+      "@eleads/shared": path.resolve(__dirname, "../shared/dist/index.js"),
     },
   },
 });
