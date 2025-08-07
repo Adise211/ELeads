ac98c06bf1cb6cc4aaea3b29ab3ad60168f64117
Adise <adisemamo211@gmail.com>
2025-08-07 10:47:48 -0400

    chore: remove unused App.css and update imports in App.tsx to use index.css

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,4 @@
-// import "@mantine/core/styles.css";
-import "./App.css";
-// import { MantineProvider } from "@mantine/core";
+import "./index.css";
 import { RouterProvider } from "react-router-dom";
 import { router } from "@/routes";
 import { ToastContainer } from "react-toastify";
@@ -9,9 +7,7 @@ import "react-toastify/dist/ReactToastify.css";
 function App() {
   return (
     <>
-      {/* <MantineProvider> */}
       <RouterProvider router={router} />
-      {/* </MantineProvider> */}
       <ToastContainer />
     </>
   );
