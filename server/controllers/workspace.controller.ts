4da6b8a3060d63a2ff3044e91ea1c9ae2f2ff807
Adise <adisemamo211@gmail.com>
2025-08-07 13:41:46 -0400

    feat: add getWorkspaceUsers controller to fetch users associated with a workspace and return safe user data

--- server/controllers/workspace.controller.ts
+++ server/controllers/workspace.controller.ts
@@ -1,6 +1,10 @@
 import { httpCodes, SuccessResponse } from "@eleads/shared";
 import { NextFunction, Request, Response } from "express";
-import { getWorkspaceLeads as getWorkspaceLeadsModel } from "../models/workspace.model.js";
+import {
+  getWorkspaceLeads as getWorkspaceLeadsModel,
+  getWorkspaceUsers as getWorkspaceUsersModel,
+} from "../models/workspace.model.js";
+import { omitFields } from "../lib/data.helper.js";
 
 export const getWorkspaceLeads = async (req: Request, res: Response, next: NextFunction) => {
   try {
@@ -10,6 +14,22 @@ export const getWorkspaceLeads = async (req: Request, res: Response, next: NextF
       success: true,
       message: "Leads fetched successfully",
       data: leads,
+    };
+    res.status(httpCodes.SUCCESS).json(successResponse);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const getWorkspaceUsers = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const { workspaceId } = (req as any).user;
+    const users = await getWorkspaceUsersModel(workspaceId);
+    const safeUsers = users.map((user) => omitFields(user, ["password"]));
+    const successResponse: SuccessResponse = {
+      success: true,
+      message: "Users fetched successfully",
+      data: safeUsers,
     };
     res.status(httpCodes.SUCCESS).json(successResponse);
   } catch (error) {
